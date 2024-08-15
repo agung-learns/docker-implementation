939ca218752d3e986d0e03b9cb328a5e8c9dbbd0
agunggapai <agung@gapai.id>
2024-08-15 10:33:10 +0700

    [NuxtJS] Initialize Project, Dockerize, docker-compose

---
+++ nuxtjs/nuxtjs-docker/app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+const appUrl = config.public.appUrl;
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div>
+      <p>APP_URL: {{ appUrl }}</p>
+    </div>
+    <NuxtWelcome />
+  </div>
+</template>
